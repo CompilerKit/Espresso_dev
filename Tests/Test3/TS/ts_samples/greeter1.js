c0a0da053a867a6ed35f8261f9bf21b68c47ba9c
enginekit <enginecollection@gmail.com>
2017-09-12 20:09:33 +0700

    268: add sample ts files

---
+++ Tests/Test3/TS/ts_samples/greeter1.js
@@ -0,0 +1,19 @@
+//sample ts
+function hello1() {
+    return "";
+}
+var Student = (function () {
+    function Student(firstName, middleInitial, lastName) {
+        this.firstName = firstName;
+        this.middleInitial = middleInitial;
+        this.lastName = lastName;
+        this.fullName = firstName + " " + middleInitial + " " + lastName;
+    }
+    return Student;
+}());
+function greeter(person) {
+    return "Hello, " + person.firstName + " " + person.lastName;
+}
+var user = new Student("Jane", "M.", "User");
+document.body.innerHTML = greeter(user);
+//# sourceMappingURL=greeter1.js.map
